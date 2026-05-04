ff6b87d8a06243e738a145e60410f95f0f5062f6
Sergio <svidaurri4@gmail.com>
2026-05-04 14:02:07 -0300

    Resuelve el ejercicio 3: Comparacion entre tipos de valor y tipos de referencia

---
+++ Dsw2026Ej8/Ejercicio3/Problema3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio3
+{
+    public class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int valorCopia = originalValue;
+            
+            valorCopia++;
+
+            Product productoCopiado = product;
+
+            productoCopiado.CambiarDescripcion("Nueva Descripcion Copiada");
+
+            return $"{originalValue}-{valorCopia}-{productoCopiado.Description}";
+        } 
+    }
+}
---
+++ Dsw2026Ej8/Ejercicio3/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio3
+{
+    public class Product
+    {
+        public string Description { get; private set; }
+        public Product(string initialDescription)
+        {
+            Description = initialDescription;
+        }
+
+        public void CambiarDescripcion(string nuevaDescription)
+        {
+            Description = nuevaDescription;
+        }
+    }
+}
