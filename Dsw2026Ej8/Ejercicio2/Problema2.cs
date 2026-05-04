6fd681c7a994922b1d5f9936a30d55cb5c4e0e32
Sergio <svidaurri4@gmail.com>
2026-05-04 13:46:26 -0300

    Resuelve el ejercicio 2: Implementacion de CrearResumenVenta con clase anonima

---
+++ Dsw2026Ej8/Ejercicio2/Problema2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio2
+{
+    public class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal totalCalculado = (quantity <= 0) ? 0 : quantity * unitPrice;
+
+            var resumen = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = totalCalculado
+            };
+
+            return $"{resumen.Code}-{resumen.Description}-{resumen.Total}";
+        }
+    }
+}
